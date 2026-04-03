3227fd44eb5f51b55296691dc91624edcbbe84d5
salina1031 <salinabhattarai09@gmail.com>
2026-04-03 20:33:59 +0545

    changing the few css properties

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -2,22 +2,24 @@ import React from 'react'
 import "../style/Skills.css"
 
 const Skills = ({ skill }) => {
-   return (
-    <section className="skill-section" id="skills">
-      <h2>Skills</h2>
+  return (
+    <div className='skill'>
+      <section className="skill-section" id="skills">
+        <h2>Skills</h2>
 
-      {Object.entries(skill).map(([category, items], index) => (
-        <div key={index} className="skill-category">
-          <h3>{category}</h3>
-          <ul>
-            {items.map((item, i) => (
-              <li key={i}>{item}</li>
-            ))}
-          </ul>
-        </div>
-      ))}
-    </section>
-  );
+        {Object.entries(skill).map(([category, items], index) => (
+          <div key={index} className="skill-category">
+            <h3>{category}</h3>
+            <ul>
+              {items.map((item, i) => (
+                <li key={i}>{item}</li>
+              ))}
+            </ul>
+          </div>
+        ))}
+      </section>
+    </div>
+      );
 };
 
 
